f0f53ec841c4fbfba4c1c0011c0c81f164359df1
Bianca Saaiman <bsaaiman13@gmail.com>
2026-07-11 15:18:08 +0200

    Add integer math operations in integermath.py
    Implemented basic integer operations including sum, difference, multiplication, and division.

---
+++ integermath.py
@@ -0,0 +1,33 @@
+# Pseudocode:
+
+#Ask user to enter three different integers
+
+# Python code:
+#Ask user for the first integer number
+num1 = input("Please enter your first integer number")
+num1 = 10
+
+# Ask user for the second integer number
+num2 = input("Please enter your second integer number")
+num2 = 4
+
+# Ask user for their third integer number
+num3 = input("Please enter your third integer number")
+num3 = 26
+
+# Sum of all the numbers
+sum = 10 + 4 + 26
+print(sum)
+
+# First number minus the second number
+number_list = [10,4]
+print(min(number_list))
+
+# Third number multiplied by the first number
+multiplication = 26 * 4
+print(multiplication)
+
+# Sum all three numbers and divide by the third number
+sum_div = 4 + 10 + 26 / 26
+print(sum_div)
+
